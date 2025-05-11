05edbda1b22e023c91f11bec5f80863614159f1b
Jyoshitha Pechetti <jyoshithapechetti03@gmail.com>
2025-05-11 15:38:09 +0530

    config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_l80EVbRbv', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6me4og5e08rjrhcfa5h4hb2geo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_yBo4MHnDY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '28s8dq63c6or49nf1vmg14ro9f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
